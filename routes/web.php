b57b89f8579e5a3eda6dd5589d33ccfdec6aa38c
= <=>
2026-05-11 13:55:11 +0630

    link to dashboard and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,5 +23,18 @@ Route::middleware('auth')->group(function(){
     Route::get('/asset',function(){
         return view('asset');
     });
+    
+        // User CRUD အတွက် UI ရော API ရော
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::post('/api/users', [UserController::class, 'store']);
+    Route::put('/api/users/{user}', [UserController::class, 'update']);
+    Route::delete('/api/users/{id}', [UserController::class, 'destroy']);
+
+    // Category CRUD အတွက် UI ရော API ရော
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::post('/api/categories', [CategoryController::class, 'store']);
+    Route::put('/api/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/api/categories/{id}', [CategoryController::class, 'destroy']);
+
 });
 
